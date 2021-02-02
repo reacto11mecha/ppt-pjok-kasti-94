dd51743c9d3383114f6a4fac9cbe091876be522c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-02-02 12:42:07 +0700

    Menambahkan slide pertama

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,13 @@
 import "webslides/static/css/webslides.css";
+import "../css/iconmoon.css";
 import "../css/style.css";
 
-import("webslides").then(() => new WebSlides());
+import("webslides").then(() => {
+  const slide = new WebSlides();
+
+  const fl = document.querySelector(".btn-fullscreen");
+  fl.addEventListener("click", function () {
+    if (slide.initialised) slide.fullscreen();
+    this.blur();
+  });
+});
