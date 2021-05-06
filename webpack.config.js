925ff76a026bf8cfbb01e3fcb95c887b2b7760f6
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-05-06 10:42:38 +0700

    Persiapan update ke webpack versi 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,138 +1,138 @@
-const path = require("path");
-const glob = require("glob");
-const TerserPlugin = require("terser-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-
-const PATHS = {
-  src: path.join(__dirname, "src"),
-  dist: path.join(__dirname, "dist"),
-};
-
-module.exports = {
-  entry: {
-    main: "./src/js/script.js",
-  },
-  output: {
-    publicPath: "",
-    path: PATHS.dist,
-    filename: "[name].bundle.js",
-  },
-  resolve: {
-    extensions: ["*", ".js"],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: "styles",
-          test: /\.css$/,
-          chunks: "all",
-          enforce: true,
-        },
-      },
-      chunks: "all",
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCSSExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-    }),
-    new FaviconsWebpackPlugin({
-      logo: "./src/img/favicon.ico",
-      devMode: "light",
-      mode: "light",
-      inject: true,
-    }),
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        plugins: [
-          ["mozjpeg", { progressive: true }],
-          ["pngquant", { optimizationLevel: 5 }],
-          [
-            "svgo",
-            {
-              plugins: [
-                {
-                  removeViewBox: false,
-                },
-              ],
-            },
-          ],
-        ],
-      },
-    }),
-    new MiniCSSExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-    new PurgecssPlugin({
-      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-      safelist: () => ({
-        standard: [
-          "webslides-zoomed",
-          "text-slide-number",
-          /^text-slide-/,
-          "navigation",
-          "zoom-layer",
-          "wrap-zoom",
-          "grid",
-          "column",
-          "previous",
-          "ws-ready",
-          "disabled",
-          "current",
-          "counter",
-          "next",
-          "in",
-        ],
-      }),
-    }),
-  ],
-};
+// const path = require("path");
+// const glob = require("glob");
+// const TerserPlugin = require("terser-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const PurgecssPlugin = require("purgecss-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+// const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+// const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+//
+// const PATHS = {
+//   src: path.join(__dirname, "src"),
+//   dist: path.join(__dirname, "dist"),
+// };
+//
+// module.exports = {
+//   entry: {
+//     main: "./src/js/script.js",
+//   },
+//   output: {
+//     publicPath: "",
+//     path: PATHS.dist,
+//     filename: "[name].bundle.js",
+//   },
+//   resolve: {
+//     extensions: ["*", ".js"],
+//   },
+//   optimization: {
+//     minimize: true,
+//     minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+//     splitChunks: {
+//       cacheGroups: {
+//         styles: {
+//           name: "styles",
+//           test: /\.css$/,
+//           chunks: "all",
+//           enforce: true,
+//         },
+//       },
+//       chunks: "all",
+//     },
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js)$/,
+//         exclude: /node_modules/,
+//         use: ["babel-loader"],
+//       },
+//       {
+//         test: /\.html$/,
+//         use: [
+//           {
+//             loader: "html-loader",
+//             options: { minimize: true },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.(png|svg|jpe?g|gif)$/,
+//         use: ["file-loader"],
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [MiniCSSExtractPlugin.loader, "css-loader"],
+//       },
+//       {
+//         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+//         use: [
+//           {
+//             loader: "file-loader",
+//             options: {
+//               name: "[name].[ext]",
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin(),
+//     new HtmlWebpackPlugin({
+//       template: "./src/index.html",
+//       filename: "./index.html",
+//     }),
+//     new FaviconsWebpackPlugin({
+//       logo: "./src/img/favicon.ico",
+//       devMode: "light",
+//       mode: "light",
+//       inject: true,
+//     }),
+//     new ImageMinimizerPlugin({
+//       minimizerOptions: {
+//         plugins: [
+//           ["mozjpeg", { progressive: true }],
+//           ["pngquant", { optimizationLevel: 5 }],
+//           [
+//             "svgo",
+//             {
+//               plugins: [
+//                 {
+//                   removeViewBox: false,
+//                 },
+//               ],
+//             },
+//           ],
+//         ],
+//       },
+//     }),
+//     new MiniCSSExtractPlugin({
+//       filename: "[name].css",
+//       chunkFilename: "[id].css",
+//     }),
+//     new PurgecssPlugin({
+//       paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+//       safelist: () => ({
+//         standard: [
+//           "webslides-zoomed",
+//           "text-slide-number",
+//           /^text-slide-/,
+//           "navigation",
+//           "zoom-layer",
+//           "wrap-zoom",
+//           "grid",
+//           "column",
+//           "previous",
+//           "ws-ready",
+//           "disabled",
+//           "current",
+//           "counter",
+//           "next",
+//           "in",
+//         ],
+//       }),
+//     }),
+//   ],
+// };
